6c1d234c803492f4333b6b3ae9a3ef353fb4c455
Son Le <sle@Sons-MacBook-Pro.local>
2015-09-08 17:58:51 -0500

    StepperMotor functionality

--- StepperMotor/StepperMotor.c
+++ StepperMotor/StepperMotor.c
@@ -5,12 +5,25 @@
  *      Author: sle
  */
 
+static int stepSequence[] = {1, 2, 4, 8};
+int position = 0;
+
 void StepperMotor_StepFoward()
 {
-
+	PORT1 = stepSequence[position];
+	if (position == 3)
+	{
+		position = 0;
+	}
+	position += 1;
 }
 
 void StepperMotor_StepBackward()
 {
-
+	PORT1 = stepSequence[position];
+	if (position == 0)
+	{
+		position = 3;
+	}
+	position -= 1;
 }
