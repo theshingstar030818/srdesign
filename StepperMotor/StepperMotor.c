12621c47514cf8fba83f2fdf706235c19d543661
Son Le <sle@dyn245-148.wireless-1240.nat.ndsu.NoDak.edu>
2015-10-15 11:01:38 -0500

    Changes to stepper motor code for forward and backward, tested and verified

--- StepperMotor/StepperMotor.c
+++ StepperMotor/StepperMotor.c
@@ -5,44 +5,103 @@
  *      Author: sle
  */
 
-//static int stepSequence[] = {1, 2, 4, 8};
-int curPosition = 0;
+void StepperMotor_Initiate()
+{
+	LPC_GPIO0->FIODIR |= (0x0000000F);
+	LPC_GPIO0->FIOPIN &= ~(0x0000000F);
+
+	currentPos = 0;
+	globalPos = 0;
+}
 
 void StepperMotor_StepFoward()
 {
-	switch (curPosition)
+	while(1)
 	{
-		case 0:
-			P0[0] = 1;
-			P0[1] = 0;
-			P0[2] = 0;
-			P0[3] = 0;
-			curPosition += 1;
+		if(isSyringeEmpty())
 			break;
-		case 1:
-			P0[0] = 0;
-			P0[1] = 1;
-			P0[2] = 0;
-			P0[3] = 0;
-			curPosition += 1;
-			break;
-		case 2:
-			P0[0] = 0;
-			P0[1] = 0;
-			P0[2] = 1;
-			P0[3] = 0;
-			curPosition += 1;
-			break;
-		case 3:
-			P0[0] = 0;
-			P0[1] = 0;
-			P0[2] = 0;
-			P0[3] = 1;
-			curPosition = 0;
-			break;
+		globalPos += 1;
+		switch(currentPos)
+		{
+			case 0:
+				LPC_GPIO0->FIOPIN = (0x00000003);
+				currentPos += 1;
+				break;
+			case 1:
+				LPC_GPIO0->FIOPIN = (0x00000002);
+				currentPos += 1;
+				break;
+			case 2:
+				LPC_GPIO0->FIOPIN = (0x00000006);
+				currentPos += 1;
+				break;
+			case 3:
+				LPC_GPIO0->FIOPIN = (0x00000004);
+				currentPos += 1;
+				break;
+			case 4:
+				LPC_GPIO0->FIOPIN = (0x0000000C);
+				currentPos += 1;
+				break;
+			case 5:
+				LPC_GPIO0->FIOPIN = (0x00000008);
+				currentPos += 1;
+				break;
+			case 6:
+				LPC_GPIO0->FIOPIN = (0x00000009);
+				currentPos += 1;
+				break;
+			case 7:
+				LPC_GPIO0->FIOPIN = (0x00000001);
+				currentPos = 0;
+				break;
+		}
+		for(i=0; i<max_time; i++);
 	}
+	stepBackward();
 }
 
 void StepperMotor_StepBackward()
 {
+	for(; globalPos > 0; globalPos--)
+	{
+		switch(currentPos)
+		{
+			case 0:
+				LPC_GPIO0->FIOPIN = (0x00000009);
+				currentPos = 7;
+				break;
+			case 1:
+				LPC_GPIO0->FIOPIN = (0x00000001);
+				currentPos--;
+				break;
+			case 2:
+				LPC_GPIO0->FIOPIN = (0x00000003);
+				currentPos--;
+				break;
+			case 3:
+				LPC_GPIO0->FIOPIN = (0x00000002);
+				currentPos--;
+				break;
+			case 4:
+				LPC_GPIO0->FIOPIN = (0x00000006);
+				currentPos--;
+				break;
+			case 5:
+				LPC_GPIO0->FIOPIN = (0x00000004);
+				currentPos--;
+				break;
+			case 6:
+				LPC_GPIO0->FIOPIN = (0x0000000C);
+				currentPos--;
+				break;
+			case 7:
+				LPC_GPIO0->FIOPIN = (0x00000008);
+				currentPos--;
+				break;
+		}
+		for(i=0; i<max_time; i++);
+	}
+	globalPos = 0;
+	stepForward();
 }
