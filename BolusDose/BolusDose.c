75599603018e004ee2805ddc08b991adefb044bf
sonnle <son.n.le@hotmail.com>
2015-11-03 22:29:29 -0600

    Split up into the separate files, and kept a single file for Keil usage

--- BolusDose/BolusDose.c
+++ BolusDose/BolusDose.c
@@ -6,3 +6,13 @@
  */
 
 
+void BolusDose_DoseInitiate(void)
+{
+
+	LPC_PINCON->PINSEL4 &=~ (3<<20); // P2.10 is GPIO
+	LPC_GPIO2->FIODIR &=~ (1<<10); // P2.10 in input
+
+	LPC_GPIOINT->IO2IntEnF |= (1 << 10); // Enabling falling edge to P2.10
+
+	NVIC_EnableIRQ(EINT3_IRQn);
+}
