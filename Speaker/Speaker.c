78bfe352d481eccc89ace9c19d0619eb37f9c075
Son Le <son.n.le@hotmail.com>
2016-01-14 11:15:18 -0600

    Speaker implementation

--- Speaker/Speaker.c
+++ Speaker/Speaker.c
@@ -4,3 +4,28 @@
  *  Created on: Jan 12, 2016
  *      Author: sle
  */
+
+void Speaker_Initiate(void)
+{
+	// Initialize pin P0.26 to output
+	LPC_GPIO->FIODIR |= 1 << 26;
+}
+
+void Speaker_Play(int loops)
+{
+	// Turn the speaker on and off no. of times specified
+	while(num_times >= 0)
+	{
+		LPC_GPIO0->FIOSET |= 1 << 26; // Set P0.26
+		Speaker_Wait();
+		LPC_GPIO0->FIOCLR |= 1 << 26; // Clear P0.26
+		Speaker_Wait();
+		num_times--; // Decrement
+	}
+}
+
+void Speaker_Wait(void) // This could possibly be moved straight into control and used as a universal wait
+{												// instead of a class specific wait loop, with parameters that define the number of loops
+	int i, j;
+	for(i = 0; i < 250000; i++);
+}
--- Speaker/Speaker.h
+++ Speaker/Speaker.h
@@ -4,3 +4,38 @@
  *  Created on: Jan 12, 2016
  *      Author: sle
  */
+ 
+#ifndef SPEAKER_SPEAKER_H_
+#define SPEAKER_SPEAKER_H_
+
+/** Speaker_Initiate()
+ *
+ *  Function is called in the main loop to initialize the Speaker pin
+ *  Set pins P0.26 as output
+ *
+ *  @param void: void 
+ *  @return void: void
+ */
+void Speaker_Initiate(void);
+ 
+/** Speaker_Initiate()
+ *
+ *  Function is called to activate the Speaker
+ *	Parameter loops dictates how long the Speaker is on for
+ *
+ *  @param int: loops - number of loops to play sound
+ *  @return void: void
+ */
+void Speaker_Play(int loops);
+
+/** Speaker_Wait()
+ *
+ *  Function is called to wait between oscillations
+ *
+ *  @param void: void
+ *  @return void: void
+ */
+void Speaker_Wait(void);
+
+#endif /* SPEAKER_SPEAKER_H_ */
+ 
