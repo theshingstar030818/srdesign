d74634480a2df67fb7870e1bc29f906fff16c876
Son Le <son.n.le@hotmail.com>
2016-01-14 11:37:10 -0600

    Bug: wrong variable name, got rid of unecessary variable

--- Speaker/Speaker.c
+++ Speaker/Speaker.c
@@ -14,18 +14,18 @@ void Speaker_Initiate(void)
 void Speaker_Play(int loops)
 {
 	// Turn the speaker on and off no. of times specified
-	while(num_times >= 0)
+	while(loops >= 0)
 	{
 		LPC_GPIO0->FIOSET |= 1 << 26; // Set P0.26
 		Speaker_Wait();
 		LPC_GPIO0->FIOCLR |= 1 << 26; // Clear P0.26
 		Speaker_Wait();
-		num_times--; // Decrement
+		loops--; // Decrement
 	}
 }
 
 void Speaker_Wait(void) // This could possibly be moved straight into control and used as a universal wait
 {												// instead of a class specific wait loop, with parameters that define the number of loops
-	int i, j;
+	int i;
 	for(i = 0; i < 250000; i++);
 }
