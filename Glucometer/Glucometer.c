8ddac0542d89cc111ecf00ed4333c9a13344b3b1
sonnle <son.n.le@hotmail.com>
2016-02-14 14:48:05 -0600

    Starting on ADC initializations

--- Glucometer/Glucometer.c
+++ Glucometer/Glucometer.c
@@ -4,4 +4,15 @@
  *  Created on: Jan 30, 2016
  *      Author: sle
  */
+
+#include "Board_ADC.h"
  
+void Glucometer_Initiate(void)
+{
+	ADC_Initialize();
+}
+
+void Glucometer_GetValue(void)
+{
+	
+}
--- Glucometer/Glucometer.h
+++ Glucometer/Glucometer.h
@@ -4,4 +4,23 @@
  *  Created on: Jan 30, 2016
  *      Author: sle
  */
+
+/** Glucometer_Initiate()
+ *
+ *  Initialize ADC
+ *
+ *  @param void: void
+ *  @return void: void
+ */
  
+void Glucometer_Initiate(void);
+
+/** Glucometer_GetValue()
+ *
+ *  Get ADC value and convert.
+ *
+ *  @param void: void
+ *  @return void: void
+ */
+ 
+void Glucometer_GetValue(void);
