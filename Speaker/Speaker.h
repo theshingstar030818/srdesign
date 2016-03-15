8670563b0fb9528e14025bbb6e813e4545791f67
sonnle <son.n.le@hotmail.com>
2016-03-15 14:09:42 -0500

    Incorporated speaker into code.

--- Speaker/Speaker.h
+++ Speaker/Speaker.h
@@ -12,9 +12,9 @@
 
 /** Speaker_Initiate()
  *
- *  Function is called in the main loop to initialize the Speaker pin
- *  Set pins P0.26 as output
- *	Initiates Timer3 but not enable
+ *  Function is called in the main loop to initialize the Speaker pin.
+ *  Set pins P0.26 as output.
+ *	Initiates Timer3 but not enable.
  *
  *  @param void: void
  *  @return void: void
@@ -22,22 +22,20 @@
 
 void Speaker_Initiate(void);
 
-/** Speaker_Initiate()
+/** Speaker_SetFrequency()
  *
- *  Function is called in the main loop to initialize the Speaker pin
- *  Set pins P0.26 as output
- *	Initiates Timer3 but not enable
+ *  Function is called to set the frequency of the speaker.
  *
  *  @param FREQ: frequency - Sets match register to FREQ value
  *  @return void: void
  */
 
-void Speaker_ChangeFrequency(FREQ frequency);
+void Speaker_SetFrequency(FREQ frequency);
 
 /** Function: Speaker_Play()
- *  
+ *
  *  Function is used to enable Timer3 counter.
- *  
+ *
  *  @param void: void
  *  @return void: void
  */
@@ -45,19 +43,19 @@ void Speaker_ChangeFrequency(FREQ frequency);
 void Speaker_Play(void);
 
 /** Function: Speaker_Stop()
- *  
+ *
  *  Function is used to disable and reset Timer3.
- *  
+ *
  *  @param void: void
  *  @return void: void
  */
- 
+
 void Speaker_Stop(void);
 
 /** Function: TIMER3_IRQHandler()
- *  
+ *
  *  Flips power to speaker.
- *  
+ *
  *  @param void: void
  *  @return void: void
  */
